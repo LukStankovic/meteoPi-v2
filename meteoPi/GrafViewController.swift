0829c1b7b80a8bf02dbfa4cd051daf9c592b8873
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-07-25 13:00:46 +0200

    Vložení grafu do prvního View

--- meteoPi/GrafViewController.swift
+++ meteoPi/GrafViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class GrafViewController: UIViewController {
 
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let htmlString:String! = "<iframe width='1200' height='708' seamless frameborder='0' scrolling='no' src='https://docs.google.com/spreadsheets/d/1E-ElrybFLzFDbhbJryf5kbKQk6J29jDipateSb64Tvs/pubchart?oid=1100784856&amp;format=interactive'></iframe>"
+        
+        /* let url = NSURL (string: "http://meteopi.stankoviclukas.cz");
+         let requestObj = NSURLRequest(URL: url!);
+         webView.loadRequest(requestObj);*/
+        
+        self.webView.frame = self.view.bounds
+        self.webView.scalesPageToFit = true
+        
+        webView.loadHTMLString(htmlString, baseURL: nil)
     }
 
     override func didReceiveMemoryWarning() {
