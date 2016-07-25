f9a1ff8cf47b126e2c3656457b04a12672cd8706
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-07-25 12:57:43 +0200

    Založení a vytvoření Views a Controllers

---
+++ meteoPi/GrafViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  meteoPi
+//
+//  Created by Lukáš Stankovič on 25.07.16.
+//  Copyright © 2016 Lukáš Stankovič. All rights reserved.
+//
+
+import UIKit
+
+class GrafViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
